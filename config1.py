08d11d8d936a6fd38a8f97105154c47700d71a4a
pijawca <devpijawca@gmail.com>
2024-06-29 16:29:35 +0700

    a

--- config1.py
+++
@@ -1,9 +0,0 @@
-TOKEN = ''
-API_KEY_MARKETCAP = ''
-db_params = {
-    'host': 'localhost', #postgres
-    'port': '5432',
-    'dbname': 'postgres',
-    'user': 'postgres',
-    'password': '' # if localhost hide
-}
