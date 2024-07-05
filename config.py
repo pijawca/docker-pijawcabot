d86dbcc709d7430df2f312b0cf9cd6e4d358a458
Ilya <73064012+pijawca@users.noreply.github.com>
2024-07-05 22:02:34 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = '#' 
+API_KEY_MARKETCAP = '#'
+db_params = {
+    'host': 'localhost',
+    'port': '5432',
+    'dbname': 'postgres',
+    'user': 'postgres',
+    'password': ''
+}
+TOTAL_MONEY = 5000
+TOTAL_TON = 7.19
